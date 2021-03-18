7c4c744dae3dff5b26a735e1a680be8a901486f7
Bongsu Jeon <bs.jeon87@gmail.com>
2021-03-18 18:26:30 +0900

    주문조회 v2 (엔티티 -> dto) : N+1문제 있음

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Order
@@ -20,11 +26,39 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    // order 조회 v1 / v2 버전 모두 쿼리가 너무 많이나감.
+    // (order select / order list를 돌면서 order별로 연관된 Memeber , Delivery select를 각각 날림.)
+    //
+
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
         //단순히 아래방시대로하면 json data 생성시 order<->member 양방향 연관관계에 의해 무한 루프에 빠짐.
         List<Order> all = orderRepository.findAllByQueryDsl(new OrderSearch());
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByQueryDsl(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 
 }
