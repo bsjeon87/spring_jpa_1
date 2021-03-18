72baf54d45e03400856cc0c8cc9b42cff30655d9
Bongsu Jeon <bs.jeon87@gmail.com>
2021-03-18 19:22:54 +0900

    주문조회 v1

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("api/v1/orders")
+    private List<Order> ordersV1() {
+        List<Order> orders = orderRepository.findAllByQueryDsl(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                orderItem.getItem().getName();
+            }
+        }
+        return orders;
+    }
+}
